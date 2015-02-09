dabe0e2458d1a7e0437ad4da7188aa16faf7e5d7
Zubkov Vova <vova.zubkov@gmail.com>
2015-02-09 11:45:55 +0300

    add drag event for index slider

--- js/source.js
+++ js/source.js
@@ -65,6 +65,16 @@ $(window).on('load',function(){
             (e.item.index == 0) ? $('.carousel-nav .prev').removeClass('active') : $('.carousel-nav .prev').addClass('active');
             (e.item.count - e.item.index == e.page.size) ? $('.carousel-nav .next').removeClass('active') : $('.carousel-nav .next').addClass('active');
         });
+    owl.on('drag.owl.carousel ',
+        function(e) {
+            //console.log($(this));
+            //console.log(e.item.index);
+            //console.log(e.item.count);
+            //console.log(e.item.size);
+            (e.item.index == 0) ? $('.carousel-nav .prev').removeClass('active') : $('.carousel-nav .prev').addClass('active');
+            (e.item.count - e.item.index == e.page.size) ? $('.carousel-nav .next').removeClass('active') : $('.carousel-nav .next').addClass('active');
+            $autoplay = false;
+        });
     owl.on('changed.owl.carousel ',
         function(e) {
             //console.log(e.item.index);
